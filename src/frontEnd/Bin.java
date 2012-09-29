b90620d674403f03010bf65dcae08f1de9ec78fa
msweatt <sweatt.marshall@gmail.com>
2012-09-28 23:30:28 -0600

    Class Structure

---
+++ src/frontEnd/Bin.java
@@ -0,0 +1,16 @@
+package frontEnd;
+
+public class Bin {
+	private int x,y;
+	private int width,height;
+	
+	public void init(){}
+	public void update(){}
+	
+	public Bin(int _x,int _y,int _w,int _h){
+		x=_x;
+		y=_y;
+		width=_w;
+		height=_h;
+	}
+}
