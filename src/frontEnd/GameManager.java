f4b1b4a71accfbc13838ca2988a88fc479686097
msweatt <sweatt.marshall@gmail.com>
2012-09-30 15:55:24 -0600

    Fixing item on screen

--- src/frontEnd/GameManager.java
+++ src/frontEnd/GameManager.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import motion.MotionDetector;
 import motion.MouseMotionDetector;
 import frontEnd.Item;
+import util.Direction;
 import util.Global;
 
 
@@ -18,20 +19,38 @@ public class GameManager {
 	private int highScore;
 	
 	public GameManager(){
-		screen = new GameScreen();;
+		screen = new GameScreen(this);;
 	}
 	public GameScreen getScreen(){
 		return screen;
 	}
 	private void addItem(Item _item){
 		items.add(_item);
+		screen.paintObject(_item);
 	}
 	private void addBin(Bin _bin){
 		bins.add(_bin);
 	}
 	
-	public void processMotion()	{
+	public void processMotion(int startX, int endX, int startY, int endY)	{
 		System.out.println("Processing motion...");
+		for(Item i : items)
+		{
+			int x = i.getX();
+			System.out.println(x);
+			if(x > startX && x < endX)
+			{
+				System.out.println("Moving1!");
+				i.update(i.getX()+100,i.getY());
+				screen.repaint();
+			}
+			if(x < startX && x > endX)
+			{
+				System.out.println("Moving2!");
+				i.update(i.getX()-100,i.getY());
+				screen.repaint();
+			}
+		}
 		
 	}
 	public void update(){}
@@ -41,6 +60,5 @@ public class GameManager {
 		GameManager game = new GameManager();
 		GameScreen screen = game.getScreen();
 		MotionDetector detector = new MouseMotionDetector(screen);
-		
 	}
 }
--- src/gui/GameScreen.java
+++ src/gui/GameScreen.java
@@ -1,24 +1,24 @@
 package gui;
 
-import frontEnd.GameManager;
-import frontEnd.Item;
-import util.Global;
-import gui.paintItem;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 
 import motion.MouseMotionDetector;
+import util.Global;
+import frontEnd.GameManager;
+import frontEnd.Item;
 
 
 public class GameScreen extends JFrame {
 	private JFrame frame;
-	private Global global;
+	private GameManager game;
 	
-	public GameScreen() {
+	public GameScreen(GameManager game) {
 		frame = new JFrame();
-		frame.setSize(global.xResolution,global.yResolution);
+		this.game = game;
+		frame.setSize(Global.xResolution,Global.yResolution);
 		frame.setVisible(true);
 	    frame.addWindowListener(new WindowAdapter() {
 	        public void windowClosing(WindowEvent e) {
@@ -27,11 +27,13 @@ public class GameScreen extends JFrame {
 	      });
 	    frame.addMouseListener(new MouseMotionDetector(this));
 	}
-	
+	public void processMotion(int startX, int endX, int startY, int endY) {
+		game.processMotion(startX, endX, startY, endY);
+	}
 	
 	public void paintObject(Item _item) {
 		// TODO Auto-generated method stub
-	    //frame.add(new PaintableItem(_item));
+	    frame.add(new PaintableItem(_item));
 	}
 	
 	public void repaint() {
